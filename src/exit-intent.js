b4a507423ff6d9fd51311a95c12551221433c7d0
Johnny Oshika <johnnyoshika@gmail.com>
2021-02-14 08:13:00 -0800

    Add top/right/bottom/left edge detection

--- src/exit-intent.js
+++ src/exit-intent.js
@@ -2,6 +2,7 @@ import throttle from 'lodash/throttle'
 
 export default function ExitIntent (options = {}) {
   const defaultOptions = {
+    sides: {top: true, right: false, bottom: false, left: false},
     threshold: 20,
     maxDisplays: 1,
     eventThrottle: 200,
@@ -9,7 +10,11 @@ export default function ExitIntent (options = {}) {
   }
 
   return (function () {
-    const config = {...defaultOptions, ...options}
+    const config = {
+      ...defaultOptions,
+      ...options,
+      sides: {...defaultOptions.sides, ...options.sides}
+    }
     const eventListeners = new Map()
     let displays = 0
 
@@ -27,16 +32,48 @@ export default function ExitIntent (options = {}) {
       eventListeners.delete(key)
     }
 
-    const shouldDisplay = (position) => {
-      if (position <= config.threshold && displays < config.maxDisplays) {
+    const shouldDisplay = (x, y) => {
+      if (
+        config.sides.top &&
+        y <= config.threshold &&
+        displays < config.maxDisplays
+      ) {
         displays++
         return true
       }
+
+      if (
+        config.sides.right &&
+        x >= viewportWidth() - config.threshold &&
+        displays < config.maxDisplays
+      ) {
+        displays++
+        return true
+      }
+
+      if (
+        config.sides.bottom &&
+        y >= viewportHeight() - config.threshold &&
+        displays < config.maxDisplays
+      ) {
+        displays++
+        return true
+      }
+
+      if (
+        config.sides.left &&
+        x <= config.threshold &&
+        displays < config.maxDisplays
+      ) {
+        displays++
+        return true
+      }
+
       return false
     }
 
     const mouseDidMove = (event) => {
-      if (shouldDisplay(event.clientY)) {
+      if (shouldDisplay(event.clientX, event.clientY)) {
         config.onExitIntent()
         if (displays >= config.maxDisplays) {
           removeEvents()
@@ -47,6 +84,18 @@ export default function ExitIntent (options = {}) {
     const removeEvents = () => {
       eventListeners.forEach((value, key, map) => removeEvent(key))
     }
+
+    // Viewport size: https://stackoverflow.com/a/8876069/188740
+    const viewportWidth = () =>
+      Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      )
+    const viewportHeight = () =>
+      Math.max(
+        document.documentElement.clientHeight || 0,
+        window.innerHeight || 0
+      )
 
     addEvent('mousemove', throttle(mouseDidMove, config.eventThrottle))
 
